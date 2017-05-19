486b934b5bd960b3f9f1e31308fc2069c7196f14
Vishal Mirchandani <vmirchandani@Vishals-MacBook-Pro.local>
2017-05-19 11:41:35 +0100

    Add countdown functionality

--- app/tests/components/CountdownForm.test.jsx
+++ app/tests/components/CountdownForm.test.jsx
@@ -6,30 +6,30 @@ var TestUtils = require("react-addons-test-utils");
 
 var CountdownForm = require("CountdownForm");
 
-describe("Clock", () => {
-  it("Should exist", () => {
-    expect(CountdownForm).toExist();
-  });
+  describe("CountdownForm", () => {
+    it("Should exist", () => {
+      expect(CountdownForm).toExist();
+    });
 
-  it("Should call onSetCountdown if valid seconds entered", () => {
-    var spy = expect.createSpy();
-    var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
-    var $el = $(ReactDOM.findDOMNode(countdownForm));
+    it("Should call onSetCountdown if valid seconds entered", () => {
+      var spy = expect.createSpy();
+      var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+      var $el = $(ReactDOM.findDOMNode(countdownForm));
 
-    countdownForm.refs.seconds.value = "109";
-    TestUtils.Simulate.submit($el.find("form")[0]);
+      countdownForm.refs.seconds.value = "109";
+      TestUtils.Simulate.submit($el.find("form")[0]);
 
-    expect(spy).toHaveBeenCalledWith(109);
-  });
+      expect(spy).toHaveBeenCalledWith(109);
+    });
 
-  it("Should not have called onSetCountdown if invalid seconds entered", () => {
-    var spy = expect.createSpy();
-    var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
-    var $el = $(ReactDOM.findDOMNode(countdownForm));
+    it("Should not have called onSetCountdown if invalid seconds entered", () => {
+      var spy = expect.createSpy();
+      var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+      var $el = $(ReactDOM.findDOMNode(countdownForm));
 
-    countdownForm.refs.seconds.value = "109b";
-    TestUtils.Simulate.submit($el.find("form")[0]);
+      countdownForm.refs.seconds.value = "109b";
+      TestUtils.Simulate.submit($el.find("form")[0]);
 
-    expect(spy).toNotHaveBeenCalled();
-  });
+      expect(spy).toNotHaveBeenCalled();
+    });
 });
